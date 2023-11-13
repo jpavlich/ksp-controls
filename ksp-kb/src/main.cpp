9a3c4e609bc1fbdabfa7df3c54f7e239ed85e288
Jaime A. Pavlich-Mariscal <jaime.pavlich@gmail.com>
2023-11-13 12:04:45 -0500

    .

--- ksp-kb/src/main.cpp
+++ ksp-kb/src/main.cpp
@@ -9,10 +9,10 @@ const int ROWS = 5;
 const int COLS = 5;
 char keys[ROWS][COLS] = {
     {KEY_KP_5, KEY_KP_0, 'r', 't', KEY_ESC},
-    {KEY_KP_8, KEY_KP_2, '6', 'c', 'd'},
-    {KEY_KP_9, KEY_KP_3, '9', 'e', 'f'},
-    {KEY_KP_6, KEY_KP_4, '#', 'g', 'h'},
-    {KEY_KP_7, KEY_KP_1, ' ', 't', 'm'}};
+    {KEY_KP_8, KEY_KP_2, KEY_KP_ENTER, '1', '2'},
+    {KEY_KP_9, KEY_KP_3, 'g', '3', '4'},
+    {KEY_KP_6, KEY_KP_4, 'b', KEY_F6, KEY_F7},
+    {KEY_KP_7, KEY_KP_1, ' ', 'm', KEY_CAPS_LOCK}};
 byte rowPins[ROWS] = {PA0, PA1, PA2, PA3, PA4};
 byte colPins[COLS] = {PB1, PB0, PA7, PA6, PA5};
 
