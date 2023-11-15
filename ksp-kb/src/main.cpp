6cd6525fbdcb4d80bdd10dbc9595bcf7c34ef214
Jaime A. Pavlich-Mariscal <jaime.pavlich@gmail.com>
2023-11-15 17:52:16 -0500

    .

--- ksp-kb/src/main.cpp
+++ ksp-kb/src/main.cpp
@@ -8,11 +8,11 @@ HIDKeyboard keyboard(HID);
 const int ROWS = 5;
 const int COLS = 5;
 char keys[ROWS][COLS] = {
-    {KEY_KP_5, KEY_KP_0, 'r', 't', KEY_ESC},
-    {KEY_KP_8, KEY_KP_2, KEY_KP_ENTER, '1', '2'},
-    {KEY_KP_9, KEY_KP_3, 'g', '3', '4'},
-    {KEY_KP_6, KEY_KP_4, 'b', KEY_F6, KEY_F7},
-    {KEY_KP_7, KEY_KP_1, ' ', 'm', KEY_CAPS_LOCK}};
+    {KEY_KP_5, KEY_KP_0, '1', '2', '3'},
+    {KEY_KP_8, KEY_KP_2, '4', '5', '6'},
+    {KEY_KP_9, KEY_KP_3, '7', '8', '9'},
+    {KEY_KP_6, KEY_KP_4, 'r', 't', 'g'},
+    {KEY_KP_7, KEY_KP_1, ' ', 'm', 'b'}};
 byte rowPins[ROWS] = {PA0, PA1, PA2, PA3, PA4};
 byte colPins[COLS] = {PB1, PB0, PA7, PA6, PA5};
 
