89e5a02189db27cbd92a8a4676ffcb0db011d8b6
Jaime A. Pavlich-Mariscal <jaime.pavlich@gmail.com>
2023-11-21 08:36:33 -0500

    .

--- ksp-control/src/main.cpp
+++ ksp-control/src/main.cpp
@@ -67,8 +67,8 @@ char keys[ROWS][COLS] = {
     {KEY_KP_5, KEY_KP_0, '1', '2', '3'},
     {KEY_KP_8, KEY_KP_2, '4', '5', '6'},
     {KEY_KP_9, KEY_KP_3, '7', '8', '9'},
-    {KEY_KP_6, KEY_KP_4, 'r', 't', 'g'},
-    {KEY_KP_7, KEY_KP_1, ' ', 'm', 'b'}};
+    {KEY_KP_6, KEY_KP_4, 'r', 't', 'b'},
+    {KEY_KP_7, KEY_KP_1, ' ', 'm', KEY_KP_ENTER}};
 byte rowPins[COLS] = {PA9, PA10, PB4, PB5, PB6};
 byte colPins[COLS] = {PB12, PB13, PB14, PB15, PA8};
 
