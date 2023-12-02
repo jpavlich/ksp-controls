d09fbc54ceee9d086f1308bd84e6161fe0c1d38a
Jaime A. Pavlich-Mariscal <jaime.pavlich@gmail.com>
2023-12-02 14:22:10 -0500

    .

--- ksp-control/src/main.cpp
+++ ksp-control/src/main.cpp
@@ -10,7 +10,15 @@
 
 #define LED_BUILTIN PB12
 
-AnalogValue<> analog_readers[6] = {
+const size_t NUM_JOYSTICKS = 2;
+const size_t NUM_AXES = 6;
+const size_t NUM_JOY_BUTTONS = 16;
+const size_t NUM_WASD_BUTTONS = 1;
+const size_t NUM_MODES = 6;
+const size_t KEYPAD_ROWS = 5;
+const size_t KEYPAD_COLS = 5;
+
+AnalogValue<> analog_readers[NUM_AXES] = {
     AnalogValue<>(PA0),
     AnalogValue<>(PA1),
     AnalogValue<>(PA2),
@@ -19,7 +27,7 @@ AnalogValue<> analog_readers[6] = {
     AnalogValue<>(PA5),
 };
 
-Fun joy_conversion[6] = {
+Fun joy_conversion[NUM_AXES] = {
     [](const float x)
     { return linear(x, 0.0, 1.0, 32767, -32767); },
     [](const float x)
@@ -33,7 +41,7 @@ Fun joy_conversion[6] = {
     [](const float x)
     { return linear(x, 0.0, 1.0, 1.0, 0.0); }};
 
-Fun wasd_conversion[6] = {
+Fun wasd_conversion[NUM_AXES] = {
     [](float x)
     { return linear(x, 0.0, 1.0, 1.0, -1.0); },
     [](float x)
@@ -48,7 +56,7 @@ Fun wasd_conversion[6] = {
     { return linear(x, 0.0, 1.0, 1.0, 0.0); },
 };
 
-int joy_buttons[16] = {
+int joy_buttons[NUM_JOY_BUTTONS] = {
     // XBox buttons are nuts. The correct button is the
     // one pointed by the '->'
     DISABLED, // 0
@@ -70,17 +78,16 @@ int joy_buttons[16] = {
 
 };
 
-int wasd_buttons[1] = {
+int wasd_buttons[NUM_WASD_BUTTONS] = {
     PB7, // 0
-
 };
 
-JoyReader<6, 16> joy_reader(analog_readers, joy_conversion, joy_buttons);
-JoyReader<6, 1> wasd_reader(analog_readers, wasd_conversion, wasd_buttons);
+JoyReader<NUM_AXES, NUM_JOY_BUTTONS> joy_reader(analog_readers, joy_conversion, joy_buttons);
+JoyReader<NUM_AXES, NUM_WASD_BUTTONS> wasd_reader(analog_readers, wasd_conversion, wasd_buttons);
 
 USBCompositeSerial compositeSerial;
 USBHID HID;
-const size_t NUM_JOYSTICKS = 2;
+
 USBMultiXBox360<NUM_JOYSTICKS> x360;
 HIDKeyboard keyboard(HID);
 
@@ -88,27 +95,25 @@ size_t current_joystick = 0;
 
 AnalogValue<> modeSelectorValue = AnalogValue<>(PA6);
 
-float mode_mean_values[6] = {171.0 / 1023.0,
-                             346.0 / 1023.0,
-                             510.0 / 1023.0,
-                             682.0 / 1023.0,
-                             842.0 / 1023.0,
-                             1.0};
+float mode_mean_values[NUM_MODES] = {171.0 / 1023.0,
+                                     346.0 / 1023.0,
+                                     510.0 / 1023.0,
+                                     682.0 / 1023.0,
+                                     842.0 / 1023.0,
+                                     1.0};
 
-ModeSelector<6> modeSelector(modeSelectorValue, mode_mean_values);
+ModeSelector<NUM_MODES> modeSelector(modeSelectorValue, mode_mean_values);
 
-const int ROWS = 5;
-const int COLS = 5;
-char keys[ROWS][COLS] = {
+char keys[KEYPAD_ROWS][KEYPAD_COLS] = {
     {KEY_KP_5, KEY_KP_0, '1', '2', '3'},
     {KEY_KP_8, KEY_KP_2, '4', '5', '6'},
     {KEY_KP_9, KEY_KP_3, '7', '8', '9'},
     {KEY_KP_6, KEY_KP_4, 'r', 't', 'b'},
     {KEY_KP_7, KEY_KP_1, ' ', 'm', KEY_KP_ENTER}};
-byte rowPins[COLS] = {PA9, PA10, PB4, PB5, PB6};
-byte colPins[COLS] = {PB12, PB13, PB14, PB15, PA8};
+byte rowPins[KEYPAD_COLS] = {PA9, PA10, PB4, PB5, PB6};
+byte colPins[KEYPAD_COLS] = {PB12, PB13, PB14, PB15, PA8};
 
-Keypad kpd = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+Keypad kpd = Keypad(makeKeymap(keys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS);
 
 void setup()
 {
@@ -132,7 +137,6 @@ void setup()
     x360.controllers[i].setManualReportMode(true);
   }
 
-  // keyboard.begin();
   Serial3.println("\nReady");
 }
 
